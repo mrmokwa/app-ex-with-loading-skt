fe1f3da8da1aa5c26be3645706eb3f9356e2273c
mrmokwa <matheus.mokwa@gmail.com>
2023-09-21 11:43:26 -0300

    remove loading indicator component

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -5,17 +5,12 @@ import { RouterModule } from '@angular/router';
 import { MatProgressBarModule } from '@angular/material/progress-bar';
 import { MatCardModule } from '@angular/material/card';
 
-import { LoadingIndicatorComponent } from './loading-indicator/loading-indicator.component';
 import { SkeletonCardComponent } from './skeleton-card/skeleton-card.component';
 import { StoreWrapperComponent } from './store-wrapper/store-wrapper.component';
 
 const MATERIAL_MODULES = [MatProgressBarModule, MatCardModule];
 
-const SHARED_COMPONENTS = [
-  LoadingIndicatorComponent,
-  SkeletonCardComponent,
-  StoreWrapperComponent,
-];
+const SHARED_COMPONENTS = [SkeletonCardComponent, StoreWrapperComponent];
 
 @NgModule({
   declarations: [SHARED_COMPONENTS],
