3d6ced21a2103c61505993c75fc5714c60665a87
mrmokwa <matheus.mokwa@gmail.com>
2023-09-21 13:31:01 -0300

    remove old loading indicator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { LoadingIndicatorService } from './shared/loading-indicator/loading-indicator.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +7,4 @@ import { LoadingIndicatorService } from './shared/loading-indicator/loading-indi
 })
 export class AppComponent {
   title = 'LoadingSkeleton';
-
-  constructor(public loadIncService: LoadingIndicatorService) {}
 }
