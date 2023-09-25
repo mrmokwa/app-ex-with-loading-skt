ed67b90858ade006f865a7b6223ee9000d760c52
mrmokwa <matheus.mokwa@gmail.com>
2023-09-25 07:50:54 -0300

    ajustart exibicao para mobile da drawer

--- src/app/shared/drawer-directive/drawer-navigation.directive.ts
+++ src/app/shared/drawer-directive/drawer-navigation.directive.ts
@@ -1,4 +1,11 @@
-import { Directive, OnDestroy, OnInit } from '@angular/core';
+import {
+  Directive,
+  ElementRef,
+  Input,
+  OnDestroy,
+  OnInit,
+  Renderer2,
+} from '@angular/core';
 import { MatDrawer } from '@angular/material/sidenav';
 import {
   BreakpointObserver,
@@ -11,18 +18,22 @@ import { Subscription } from 'rxjs';
   selector: '[appDrawerNavigation]',
 })
 export class DrawerNavigationDirective implements OnInit, OnDestroy {
+  @Input() size = '330px';
+
   private subscription = new Subscription();
 
   constructor(
     private matDrawer: MatDrawer,
-    private observer: BreakpointObserver
+    private observer: BreakpointObserver,
+    private elRef: ElementRef,
+    private renderer: Renderer2
   ) {}
 
   ngOnInit(): void {
     this.matDrawer.position = 'end';
 
     this.subscription = this.observer
-      .observe([Breakpoints.Small, Breakpoints.XSmall])
+      .observe([Breakpoints.XSmall])
       .subscribe((state) => this.updateView(state));
   }
 
@@ -30,9 +41,11 @@ export class DrawerNavigationDirective implements OnInit, OnDestroy {
     if (state.matches) {
       this.matDrawer.mode = 'over';
       this.matDrawer.disableClose = false;
+      this.renderer.setStyle(this.elRef.nativeElement, 'width', '100%');
     } else {
       this.matDrawer.mode = 'side';
       this.matDrawer.disableClose = true;
+      this.renderer.setStyle(this.elRef.nativeElement, 'width', this.size);
     }
   }
 
